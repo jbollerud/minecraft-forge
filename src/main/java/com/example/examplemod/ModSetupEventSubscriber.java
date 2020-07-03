3ee63929c3cae56418ffe2884c6e01f1fc59db7c
joshua <joshua@bollosoft.com>
2020-07-03 14:19:59 -0700

    add initial ingot

---
+++ src/main/java/com/example/examplemod/ModSetupEventSubscriber.java
@@ -0,0 +1,27 @@
+package com.example.examplemod;
+
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModSetupEventSubscriber {
+    @SubscribeEvent
+    public static void onRegisterItems(RegistryEvent.Register<Item> event) {
+        event.getRegistry().registerAll(
+                setup(new Item(new Item.Properties()), "epic_ingot")
+        );
+    }
+
+    public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final String name) {
+        return setup(entry, new ResourceLocation(Main.MODID, name));
+    }
+
+    public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final ResourceLocation registryName) {
+        entry.setRegistryName(registryName);
+        return entry;
+    }
+}
